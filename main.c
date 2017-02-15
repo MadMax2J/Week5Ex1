b23186a3a50ae39e65b59cefc19699de50a75265
John Byrne <usscork@gmail.com>
2017-02-15 09:15:12 +0000

    beta update

--- main.c
+++ main.c
@@ -1,18 +1,29 @@
+
+//Access the C Standard Library
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <mem.h>
 
+//Don't know why, but the header name is different on a linux system??
+#ifdef __linux__
+    #include <memory.h>
+#else
+    #include <mem.h>
+#endif
+
+//Preprocessor constant definitions
 #define NUMBER_OF_DATAPOINTS 1440
 #define INPUT_BUFFER 64
 #define RAW_DATA_FILE "../20130922WindData.csv"
 #define SORTED_OUTPUT_FILE "../sortedData.dat"
 
-void printArrayContent(float arrayToPrint[]);
-void insertionSort(float unsortedArray[]);
-void bubbleSort(float unsortedArray[]);
-void writeSortedDataToFile(float dataToWriteToFile[], size_t i);
+//Function Prototypes
 void loadFileData(float *windSpeeds, size_t arraySize);
+void bubbleSort(float unsortedArray[]);
+void insertionSort(float unsortedArray[]);
+void writeSortedDataToFile(float dataToWriteToFile[], size_t i);
+void printArrayContent(float arrayToPrint[]);
+
 
 int main() {
     //Need two arrays of unsorted data, in order to compare the performance of my two sorting algorithms.
@@ -44,94 +55,46 @@ int main() {
 
 }//End of main()
 
-void loadFileData(float *windSpeeds, size_t arraySize){
+
+void loadFileData(float *windSpeeds, size_t arraySize) {
 
     FILE *inputPtr;     //A pointer to an input file
 
     //Try open the file...
-    if((inputPtr = fopen(RAW_DATA_FILE, "r")) == NULL){ //Opening Failed?
+    if ((inputPtr = fopen(RAW_DATA_FILE, "r")) == NULL) { //Opening Failed?
         puts("The file could not be opened!");
 
     } else {    //Opening Success!
         size_t index = 0;                   //A counter to iterate through the array.
         char buffer[INPUT_BUFFER] = "";     //A buffer to accept a line read in from the input file.
 
-        fscanf( inputPtr, "%s", buffer);    //Read in the first line of the file
-        while( !feof( inputPtr ) && index < arraySize){  //Once there is data and we have'nt exceeded our array
+        fscanf(inputPtr, "%s", buffer);    //Read in the first line of the file
+        while (!feof(inputPtr) && index < arraySize) {  //Once there is data and we have'nt exceeded our array
 
             //Strings are in the following format... "9/22/2013,00:01,3.9758"
-            strtok (buffer," ,");   //Dump first token, ie. '9/22/2013'
-            strtok (NULL, " ,");    //Dump next token, ie. '00:01'
-            windSpeeds[index++] = strtof((strtok (NULL, " ,")), NULL);
+            strtok(buffer, " ,");   //Dump first token, ie. '9/22/2013'
+            strtok(NULL, " ,");    //Dump next token, ie. '00:01'
+            windSpeeds[index++] = strtof((strtok(NULL, " ,")), NULL);
 
             ////TESTING
             //printf("Data %d is: %f\n", index - 1, windSpeeds[index-1]);
 
-            fscanf( inputPtr, "%s", buffer);    //Read in the next line in the file
+            fscanf(inputPtr, "%s", buffer);    //Read in the next line in the file
 
         }   //Back to the while condition
 
         //If the inputPtr contains the EOF character, the while loop finishes and we add the last piece of data...
-        strtok (buffer," ,");   //Dump first token, ie. '9/22/2013'
-        strtok (NULL, " ,");    //Dump next token, ie. '00:01'
-        windSpeeds[index] = strtof((strtok (NULL, " ,")), NULL);
+        strtok(buffer, " ,");   //Dump first token, ie. '9/22/2013'
+        strtok(NULL, " ,");    //Dump next token, ie. '00:01'
+        windSpeeds[index] = strtof((strtok(NULL, " ,")), NULL);
         ////TESTING
         //printf("Data %d is: %f\n", index, windSpeeds[index]);
 
 
-        fclose( inputPtr ); //When finished, Close the file.
+        fclose(inputPtr); //When finished, Close the file.
     }
 
 }
-
-
-void writeSortedDataToFile(float dataToWriteToFile[], size_t size) {
-    FILE *outputFile;
-
-    if((outputFile = fopen(SORTED_OUTPUT_FILE, "w")) == NULL){
-        puts("Failed to Open file for Writing!");
-    }else{
-        for(size_t index = 0; index < size; index++){
-            fprintf(outputFile, "%f\n", dataToWriteToFile[index]);
-        }
-    }
-
-    fclose(outputFile);
-
-}
-
-void insertionSort(float unsortedArray[]) {
-    clock_t startTime = clock();
-    unsigned int copyCount = 0;
-    unsigned int insertionCount = 0;
-
-
-    size_t in, out;
-
-    for(out = 1; out < NUMBER_OF_DATAPOINTS; out++){
-
-        float temp = unsortedArray[out]; //remove market item //Why not 0??
-        in = out;
-
-        while(in > 0 && unsortedArray[in-1] >= temp){
-            copyCount++;
-            unsortedArray[in] = unsortedArray[in-1];
-            --in;
-        }
-        insertionCount++;
-        unsortedArray[in] = temp;
-
-
-    }
-    clock_t finishTime = clock();
-
-    printf("Insertion Sort completed after \t%f seconds, with %7u data copies and %8u data insertions.\n",
-           (double)(finishTime - startTime) / CLOCKS_PER_SEC, copyCount, insertionCount);
-    //printf("Time taken for Insertion Sort was: %f!\n\n\n", (double)(finishTime - startTime) / CLOCKS_PER_SEC);
-
-}
-
-
 
 void bubbleSort(float unsortedArray[]) {
 
@@ -147,19 +110,19 @@ void bubbleSort(float unsortedArray[]) {
 
     /* bubble sort */
     /* loop to control number of passes */
-    for ( pass = 1; pass < NUMBER_OF_DATAPOINTS; pass++ ) {
+    for (pass = 1; pass < NUMBER_OF_DATAPOINTS; pass++) {
 
         /* loop to control number of comparisons per pass */
-        for ( i = 0; i < NUMBER_OF_DATAPOINTS - 1; i++ ) {
+        for (i = 0; i < NUMBER_OF_DATAPOINTS - 1; i++) {
 
             /* compare adjacent elements and swap them if first
             element is greater than second element */
             comparisonCount++;
-            if ( unsortedArray[ i ] > unsortedArray[ i + 1 ] ) {
+            if (unsortedArray[i] > unsortedArray[i + 1]) {
                 swapCount++;
-                hold = unsortedArray[ i ];
-                unsortedArray[ i ] = unsortedArray[ i + 1 ];
-                unsortedArray[ i + 1 ] = hold;
+                hold = unsortedArray[i];
+                unsortedArray[i] = unsortedArray[i + 1];
+                unsortedArray[i + 1] = hold;
             } /* end if */
         } /* end inner for */
     } /* end outer for */
@@ -167,9 +130,58 @@ void bubbleSort(float unsortedArray[]) {
     clock_t finishTime = clock();
 
     printf("Bubble Sort completed after \t%f seconds, with %7u data swaps and  %8u data comparisons.\n",
-           (double)(finishTime - startTime) / CLOCKS_PER_SEC, swapCount, comparisonCount);
+           (double) (finishTime - startTime) / CLOCKS_PER_SEC, swapCount, comparisonCount);
 
     //printf("Time taken for Bubble Sort was: %f!\n\n\n", (double)(finishTime - startTime) / CLOCKS_PER_SEC);
+
+}
+
+
+void insertionSort(float unsortedArray[]) {
+    clock_t startTime = clock();
+    unsigned int copyCount = 0;
+    unsigned int insertionCount = 0;
+
+
+    size_t in, out;
+
+    for (out = 1; out < NUMBER_OF_DATAPOINTS; out++) {
+
+        float temp = unsortedArray[out]; //remove market item //Why not 0??
+        in = out;
+
+        while (in > 0 && unsortedArray[in - 1] >= temp) {
+            copyCount++;
+            unsortedArray[in] = unsortedArray[in - 1];
+            --in;
+        }
+        insertionCount++;
+        unsortedArray[in] = temp;
+
+
+    }
+    clock_t finishTime = clock();
+
+    printf("Insertion Sort completed after \t%f seconds, with %7u data copies and %8u data insertions.\n",
+           (double) (finishTime - startTime) / CLOCKS_PER_SEC, copyCount, insertionCount);
+    //printf("Time taken for Insertion Sort was: %f!\n\n\n", (double)(finishTime - startTime) / CLOCKS_PER_SEC);
+
+}
+
+
+
+void writeSortedDataToFile(float dataToWriteToFile[], size_t size) {
+    FILE *outputFile;
+
+    if ((outputFile = fopen(SORTED_OUTPUT_FILE, "w")) == NULL) {
+        puts("Failed to Open file for Writing!");
+    } else {
+        for (size_t index = 0; index < size; index++) {
+            fprintf(outputFile, "%f\n", dataToWriteToFile[index]);
+        }
+    }
+
+    fclose(outputFile);
 
 }
 
